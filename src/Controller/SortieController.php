992b3673ed84358dc7390723f0662453a630219d
Alexis <kofskin@hotmail.fr>
2025-04-07 16:16:19 +0200

    [Tache #2] création du controller et de ses méthodes CRUD

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -44,5 +44,44 @@ final class SortieController extends AbstractController
 
 	}
 
+	#[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
+	public function show(Sortie $sortie): Response
+	{
+		if (!$sortie) {
+			throw $this->createNotFoundException('Sortie inconnue');
+		}
+		return $this->render('sortie/detail.html.twig', [
+			'sortie' => $sortie,
+		]);
+	}
+
+	#[Route('/{id}/editer', name: 'editer', methods: ['GET', 'POST'])]
+	public function edit(Request $request, Sortie $sortie, EntityManagerInterface $em): Response
+	{
+		$form = $this->createForm(SortieType::class, $sortie);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$em->flush();
+
+			return $this->redirectToRoute('sortie_index', [], Response::HTTP_SEE_OTHER);
+		}
+
+		return $this->render('sortie/edit.html.twig', [
+			'sortie' => $sortie,
+			'form' => $form,
+		]);
+	}
+
+	#[Route('/{id}', name: 'supprimer', methods: ['POST'])]
+	public function delete(Request $request, Sortie $sortie, EntityManagerInterface $em): Response
+	{
+		if ($this->isCsrfTokenValid('delete'.$sortie->getId(), $request->getPayload()->getString('_token'))) {
+			$em->remove($sortie);
+			$em->flush();
+		}
+
+		return $this->redirectToRoute('sortie_index', [], Response::HTTP_SEE_OTHER);
+	}
 
 }
