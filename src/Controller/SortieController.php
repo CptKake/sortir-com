5a7dc798f5bd0adf884cc5fb856bf81d7e5f6841
Alexis <kofskin@hotmail.fr>
2025-04-08 09:23:32 +0200

    WIP: Sauvegarde du travail en cours

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -27,6 +29,9 @@ final class SortieController extends AbstractController
 	public function create(Request $request, EntityManagerInterface $em): Response
 	{
 		$sortie = new Sortie();
+		$user = $this->getUser();
+		dd($user);
+		$sortie->setOrganisateur($user);
 		$form = $this->createForm(SortieType::class, $sortie);
 		$form->handleRequest($request);
 
